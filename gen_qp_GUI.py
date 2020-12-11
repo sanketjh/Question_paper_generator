d4957739e91d07758bdaab27e348d505b3203e35
sjshetti <sanket151001@gmail.com>
2020-12-11 09:08:31 +0530

    New GUI where fields can be directly edited

--- gen_qp_GUI.py
+++ gen_qp_GUI.py
@@ -1,18 +1,20 @@
 '''
+GUI for displaying and editing table contents.
 Assumption- Column Names: qno, question_text, answers, paramaters
-This is a basic GUI program which displays the contents of a table and opens its field
-in an editor.
-To select a row in the table, click on the corresponding row number.
-To open a field in a text editor, select a row and click on the Markdown button
+The initial window allows the user to choose the database.
+In the main window, the user can edit the content of the table.
+The main window also has a menu at the top.
+File > Open allows the user to open another database
+File > Exit allows the user to exit the application
 '''
 import PySimpleGUIQt as sg
 import sqlite3
 
-sg.theme('Reddit')  # please make your windows colorful
+sg.theme('Reddit')
 
-#----Initial Window - asks for the source of database
+#Initial layout for choosing database
 layout1 = [[sg.Text('Source for database ', size=(15, 1)), sg.InputText(), sg.FileBrowse()],
-      [sg.Stretch(),sg.Submit(), sg.Cancel(),sg.Stretch()]]
+      [sg.Submit(), sg.Cancel()]]
 
 window1 = sg.Window('Choose the database', layout1)
 
@@ -23,69 +25,88 @@ conn = sqlite3.connect(file_path)   #Opens connection to the database
 cursor = conn.cursor()                   #To read data from the table
 
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tableName = cursor.fetchall()[0][0]   #Name of the table in the database
+tableName = cursor.fetchall()[0][0]
 window1.close()
-
-
-# ----Main Window which displays the table
 data = cursor.execute("SELECT * FROM %s" %tableName).fetchall()
 data=[list(i) for i in data]
-layout = [[sg.Stretch()],[sg.Stretch(), sg.Table(values=data, headings=['qno','question_text','answers','parameters'],
-                    # background_color='light blue',
-                    auto_size_columns=True,
-                    display_row_numbers=False,
-                    justification='right',
-                    num_rows=len(data),
-                    key='-TABLE-',
-                    #row_height=35,
-                    tooltip='This is a table'),sg.Stretch()],#[sg.Stretch()],
-          [sg.Stretch(),sg.Button('Markdown'),sg.Stretch()],
-          [sg.Text('Markdown = Renders the selected row in markdown'),sg.Stretch()],
-          [sg.Text('To select a row, click on the row number'), sg.Stretch()],[sg.Stretch()]]
+numColumns = 4
+numRows = len(data)
 
-window = sg.Window('The Table Element', layout,
-                   # font='Helvetica 25',
-                   ).Finalize()
+def TableSimulation():
+    """
+    Display data in a table format
+    """
+    sg.set_options(element_padding=(0, 0))
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED:
-        break
-    if event == 'Markdown':
-        if not values['-TABLE-']:
-            sg.Popup('Please select a row first',keep_on_top= True)
-            continue
-        editLayout =[[sg.Stretch()],[sg.Text('Which field do you want to render in Markdown?')],
-        [sg.Stretch(),sg.Button('qno'),sg.Stretch()],
-        [sg.Stretch(),sg.Button('question_text'),sg.Stretch()],
-        [sg.Stretch(),sg.Button('answers'),sg.Stretch()],
-        [sg.Stretch(),sg.Button('paramaters'),sg.Stretch()],[sg.Stretch()]]
-        editWindow = sg.Window('Edit', editLayout)
-        editEvent, editValue = editWindow.read()
-        editWindow.close()
-        if editEvent == 'qno':
-            editLayout1 =[[sg.Multiline(str(data[int(values['-TABLE-'][0])][0]), pad=(
-                        1, 1))],[sg.Stretch(),sg.Button('Done'),sg.Stretch()]]
-            editWindow1 = sg.Window('Edit qno', editLayout1)
-            editEvent1, editValue1 = editWindow1.read()
-            editWindow1.close()
-        elif editEvent == 'question_text':
-            editLayout1 =[[sg.Multiline(str(data[int(values['-TABLE-'][0])][1]),pad=(
-                        1, 1))],[sg.Stretch(),sg.Button('Done'),sg.Stretch()]]
-            editWindow1 = sg.Window('Edit question_text', editLayout1)
-            editEvent1, editValue1 = editWindow1.read()
-            editWindow1.close()
-        elif editEvent == 'answers':
-            editLayout1 =[[sg.Multiline(str(data[int(values['-TABLE-'][0])][2]), pad=(
-                        1, 1))],[sg.Stretch(),sg.Button('Done'),sg.Stretch()]]
-            editWindow1 = sg.Window('Edit answers', editLayout1)
-            editEvent1, editValue1 = editWindow1.read()
-            editWindow1.close()
-        elif editEvent == 'paramaters':
-            editLayout1 =[[sg.Multiline(str(data[int(values['-TABLE-'][0])][3]), pad=(
-                        1, 1))],[sg.Stretch(),sg.Button('Done'),sg.Stretch()]]    #, justification='left' removed
-            editWindow1 = sg.Window('Edit paramaters', editLayout1)
-            editEvent1, editValue1 = editWindow1.read()
-            editWindow1.close()
-window.close()
+    menu_def = [['File', ['Open', 'Exit']],    #removed  'Save',
+                #['Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
+                ['Help', 'About...'], ]
+
+    MAX_ROWS = numRows
+    MAX_COL = numColumns
+    global data
+
+    #Layout of the table
+    columm_layout =  [[sg.Multiline(str(data[i][j]),size=(25, 5), pad=(
+            1, 1), key=(i, j)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
+    col_layout = [[sg.Text('qno'),sg.Text('question_text'),sg.Text('answers'),sg.Text('paramaters')]]
+    col_layout.extend(columm_layout)
+
+    #Layout of the table window
+    layout = [[sg.Menu(menu_def)],[sg.Stretch()],
+              [sg.Stretch(),sg.Text('Table of Questions and Solutions', font='Any 18'),sg.Stretch()],
+              [sg.Stretch(),sg.Col(col_layout, size=(800,600),scrollable=True),sg.Stretch()],[sg.Stretch()]]
+
+    window = sg.Window('Table', layout, return_keyboard_events=True)
+
+    while True:
+        event, values = window.read()
+        # --- Process buttons --- #
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        elif event == 'About...':
+            sg.popup('Demo of table capabilities')
+        elif event == 'Open':
+            filename = sg.popup_get_file(
+                'Choose the database to open', no_window=True)
+            # --- populate table with file contents --- #
+            if filename is not None:
+                global conn
+                conn.close()
+                conn = sqlite3.connect(filename)   #Opens connection to the database
+                cursor = conn.cursor()                   #To read data from the table
+                cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+                try:
+                    tableName = cursor.fetchall()[0][0]
+                except:
+                    sg.popup_ok('No file selected')
+                    continue
+                try:
+                        data = cursor.execute("SELECT * FROM %s" %tableName).fetchall()
+                        data=[list(i) for i in data]
+                        MAX_ROWS = len(data)
+                except:
+                        sg.popup_error('Error reading file')
+                        continue
+                # clear the table window
+                window.close()
+
+                #New layout with new data
+                columm_layout =  [[sg.Multiline(str(data[i][j]),size=(25, 5), pad=(
+                        1, 1), key=(i, j)) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
+                col_layout = [[sg.Text('qno'),sg.Text('question_text'),sg.Text('answers'),sg.Text('paramaters')]]
+                col_layout.extend(columm_layout)
+
+
+                layout = [[sg.Menu(menu_def)],[sg.Stretch()],
+                          [sg.Stretch(),sg.Text('Table of Questions and Solutions', font='Any 18'),sg.Stretch()],
+                          [sg.Stretch(),sg.Col(col_layout, size=(800,600),scrollable=True),sg.Stretch()],[sg.Stretch()]]
+
+                window = sg.Window('Table', layout, return_keyboard_events=True)
+            else:
+                continue
+
+    window.close()
+
+
+TableSimulation()
